280b6791279c672cdd3d109ed101a992bdde0104
Robbert <robbertbw@gmail.com>
2020-11-06 11:52:49 +0100

    Add script for look at

---
+++ Assets/Script/AnimatorLookAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorLookAt : MonoBehaviour
+{
+    public Transform focus;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetLookAtWeight(1);
+        anim.SetLookAtPosition(focus.position);
+    }
+}
