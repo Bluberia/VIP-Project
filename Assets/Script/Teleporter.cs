43f3850f980c867b1a342d39c3037953976312c5
Robbert <robbert.bockweg@hightechict.nl>
2020-11-09 20:03:27 +0100

    Added buttons

---
+++ Assets/Script/Teleporter.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+
+    public Transform camping;
+    public Transform water;
+    public Transform player;
+
+    public void TeleportCamping() {
+        player.position = camping.position;
+    }
+
+    public void TeleportWater() {
+        player.position = water.position;
+    }
+}
